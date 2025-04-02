c66d5059afb129e06ae971ac419c617482f3528d
Mi-Kyung331 <uu.3120@gmail.com>
2025-04-02 21:32:01 +0900

    20250402: git fetch와 git pull은 중요합니다.

---
+++ 01-starting-project/src/App3.jsx
@@ -0,0 +1,45 @@
+
+import { CORE_CONCEPTS } from "./data";
+import Header2 from "./components/Header/Header2";
+import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
+import TabButton from "./components/TabButtons";
+
+function App() {
+  return (
+    <div>
+      <Header2 />
+      <main>
+        <section id="core-concepts">
+          <h2>핵심 개념들 of React</h2>
+          <ul>
+            <CoreConcepts 
+              image={CORE_CONCEPTS[0].image} 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description} />
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+            <CoreConcepts />
+            <CoreConcepts />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>예시들</h2>
+          <menu>
+            {/* <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton> */}
+            <TabButton label='Components' />
+            <TabButton label='JSX' />
+            <TabButton label='Props' />
+            <TabButton label='State' />
+          </menu>
+          동적인 버튼 클릭의 결과문
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- 10000_hours_rule/js/10000hours.js
+++ 10000_hours_rule/js/10000hours.js
@@ -1,25 +1,26 @@
+
 const startButton = document.querySelector(".start_btn");
-const openButton = document.querySelector(".modal_btn");
-const closeButton = document.querySelector(".close_btn");
 const result = document.querySelector(".result");
 const modal = document.querySelector("#modal");
+const openButton = document.querySelector(".modal_btn");
+const closeButton = document.querySelector(".close_btn");
 const shareButton = document.querySelector(".share_btn");
 const loading = document.querySelector(".result_loading");
 
 function calculator() {
   const fieldValue = document.querySelector("#field_value");
   const timeValue = document.querySelector("#time_value");
-  let timeValue_int = Number(timeValue.value);
+  const timeValue_int = Number(timeValue.value);
 
   const fieldResult = document.querySelector(".field_result");
   const timeResult = document.querySelector(".time_result");
 
   if(fieldValue.value == "") {
-    alert('입력되지 않았습니다.');
+    alert('분야가 입력되지 않았습니다.');
     fieldValue.focus();
     return false;
   } else if(timeValue.value == ""){
-    alert('입력되지 않았습니다.');
+    alert('시간이 입력되지 않았습니다.');
     timeValue.focus();
     return false;
   } else if(timeValue_int > 24) {
@@ -31,10 +32,10 @@ function calculator() {
   loading.style.display = 'flex';
 
   setTimeout(function() {
-    loading.style.display = 'none';
-    result.style.display = 'flex';
     fieldResult.innerText = fieldValue.value;
     timeResult.innerText = parseInt((10000/timeValue_int), 10);
+    loading.style.display = 'none';
+    result.style.display = 'flex';
   }, 1800);
 }
 
